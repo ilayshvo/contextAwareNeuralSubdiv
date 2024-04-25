afa71e2c85cb0373202f06fbad686270fd6b77d9
ilays <ilayshvo@gmail.com>
2024-04-25 15:28:12 +0200

    fixing test.py

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ from __future__ import print_function
 
 from include import *
 from models import *
+
 NETPARAMS = 'netparams.dat'
+
 
 # run the file as "python test.py ./path/to/folder/ ./path/to/testMesh.obj"
 # WARNING
@@ -15,7 +17,6 @@ NETPARAMS = 'netparams.dat'
 #   (we include a failure case in the data: the ear of the horse.obj)
 
 def main():
-
     # load hyper parameters
     folder = sys.argv[1]
     with open(folder + 'hyperparameters.json', 'r') as f:
@@ -40,27 +41,28 @@ def main():
 
     # write output shapes (test set)
     mIdx = 0
-    scale = 1.0 # may need to adjust the scale of the mesh since the network is not scale invariant
-    meshName = os.path.basename(sys.argv[2])[:-4] # meshName: "bunny"
+    scale = 1.0  # may need to adjust the scale of the mesh since the network is not scale invariant
+    meshName = os.path.basename(sys.argv[2])[:-4]  # meshName: "bunny"
     x = T.getInputData(mIdx)
-    outputs = net(x[0], x[1], mIdx,T.hfList,T.poolMats,T.dofs)
+    outputs = net(x[0], x[1], mIdx, T.hfList, T.poolMats, T.dofs)
     for ii in range(len(outputs)):
         x = outputs[ii].cpu() * scale
-        tgp.writeOBJ(params['output_path'] + meshName + '_subd' + str(ii) + '.obj',x, T.meshes[mIdx][ii].F.to('cpu'))
+        tgp.writeOBJ(params['output_path'] + meshName + '_subd' + str(ii) + '.obj', x, T.meshes[mIdx][ii].F.to('cpu'))
 
     # write rotated output shapes
     x = T.getInputData(mIdx)
 
     dV = torch.rand(1, 3).to(params['device'])
     R = random3DRotation().to(params['device'])
-    x[0][:,:3] = x[0][:,:3].mm(R.t())
-    x[0][:,3:] = x[0][:,3:].mm(R.t())
-    x[0][:,:3] += dV
-    outputs = net(x, mIdx,T.hfList,T.poolMats,T.dofs) 
+    x[0][:, :3] = x[0][:, :3].mm(R.t())
+    x[0][:, 3:] = x[0][:, 3:].mm(R.t())
+    x[0][:, :3] += dV
+    outputs = net(x[0], x[1], mIdx, T.hfList, T.poolMats, T.dofs)
 
     for ii in range(len(outputs)):
         x = outputs[ii].cpu() * scale
-        tgp.writeOBJ(params['output_path'] + meshName + '_rot_subd' + str(ii) + '.obj',x, T.meshes[mIdx][ii].F.to('cpu'))
+        tgp.writeOBJ(params['output_path'] + meshName + '_rot_subd' + str(ii) + '.obj', x,
+                     T.meshes[mIdx][ii].F.to('cpu'))
 
 
 if __name__ == '__main__':
